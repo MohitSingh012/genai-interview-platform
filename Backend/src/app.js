3d87e0000f1d6914259ac6c8349a30d6c456536f
MohitSingh012 <ms9936642@gmail.com>
2026-07-04 18:15:58 +0530

    Update CORS for frontend

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -7,7 +7,9 @@ const cors = require("cors");
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-    origin:"http://localhost:5173",
+    origin:["http://localhost:5173",
+        "https://genai-backend-s7cu.onrender.com"
+    ],
     credentials:true 
 }));
 // require all the routes here
