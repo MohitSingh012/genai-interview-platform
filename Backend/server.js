d98fc778ad8e6878b90effbf16d8ae740662c02d
MohitSingh012 <ms9936642@gmail.com>
2026-07-04 17:27:47 +0530

    Add health check route

--- Backend/server.js
+++ Backend/server.js
@@ -1,6 +1,9 @@
 const app = require("./src/app");
 const connectToDB = require("./src/config/database");
 
+app.get("/", (req, res) => {
+  res.send("Backend is running successfully ");
+});
 connectToDB()
 app.listen(process.env.PORT,()=>{
     console.log(`server is running on port`,process.env.PORT);
