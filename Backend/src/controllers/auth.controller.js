8983c64deb170764f3760b8ea75f6db95bbf6711
MohitSingh012 <ms9936642@gmail.com>
2026-07-14 10:58:05 +0530

    chore: clean up debug comments and fix register status code

--- Backend/src/controllers/auth.controller.js
+++ Backend/src/controllers/auth.controller.js
@@ -51,9 +51,9 @@ async function registerUserController(req, res) {
     maxAge: 24 * 60 * 60 * 1000,
 });
 
-  res.status(200).json({
+  res.status(201).json({
     message: "User registered successfully",
-      token, // add this line
+      token,
 
     user: {
       id: user._id,
@@ -108,7 +108,7 @@ async function loginUserController(req, res) {
 
   res.status(200).json({
     message:"User logged in successfully",
-      token, // add this line
+      token, 
 
     user:{
         id:user._id,
